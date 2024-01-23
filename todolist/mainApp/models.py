15c92040e813202df0b42ad90afde4faf383f368
Yaswanth <Yaswanthps777@gmail.com>
2024-01-23 13:47:33 +0530

    fourth commit

--- todolist/mainApp/models.py
+++ todolist/mainApp/models.py
@@ -1,8 +1,8 @@
-# todo/models.py
+# mainApp/models.py
 from django.db import models
 
 class Todo(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, default='Default Title')
     description = models.TextField(blank=True)
     order = models.IntegerField(default=0)
     completed = models.BooleanField(default=False)
