1825a06b6dac38bf88386241187ceb8059ff3020
Yaswanth <Yaswanthps777@gmail.com>
2024-01-26 10:52:09 +0530

    changes updated

--- todolist/mainApp/urls.py
+++ todolist/mainApp/urls.py
@@ -1,12 +1,7 @@
-# todo/urls.py
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import TodoViewSet
-
-router = DefaultRouter()
-router.register(r'todos', TodoViewSet, basename='todo')
+from django.urls import path
+from .views import TodoAPI
 
 urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api/todos/create/', TodoViewSet.as_view({'post': 'create_todo'}), name='create_todo'),
+    path('todos/', TodoAPI.as_view(), name='todo-list'),  # For creating todos
+    path('todos/<int:pk>/', TodoAPI.as_view(), name='todo-detail'),  # For retrieving, updating, deleting todos
 ]
--- todolist/mainApp/views.py
+++ todolist/mainApp/views.py
@@ -1,59 +1,47 @@
-# todo/views.py
-from rest_framework import viewsets
-from rest_framework.decorators import action
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Todo
 from .serializers import TodoSerializer
 
-class TodoViewSet(viewsets.ModelViewSet):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+class TodoAPI(APIView):
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=False, methods=['post'])
-    def create_todo(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    @action(detail=True, methods=['delete'])
-    def delete_todo(self, request, pk=None):
-        todo = self.get_object()
+    def delete(self, request, pk):
+        try:
+            todo = Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            return Response({'detail': 'Todo not found'}, status=status.HTTP_404_NOT_FOUND)
+        
         todo.delete()
         return Response({'detail': 'Todo deleted successfully'})
 
-    @action(detail=True, methods=['put'])
-    def reorder(self, request, pk=None):
-        todo = self.get_object()
-        order = request.data.get('order')
+    def put(self, request, pk):
+        try:
+            todo = Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            return Response({'detail': 'Todo not found'}, status=status.HTTP_404_NOT_FOUND)
 
+        serializer = TodoSerializer(todo, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk):
+        try:
+            todo = Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            return Response({'detail': 'Todo not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        order = request.data.get('order')
         if order is not None:
             todo.order = order
             todo.save()
-
-        return Response({'detail': 'Todo reordered successfully'})
-
-    @action(detail=True, methods=['put'])
-    def edit(self, request, pk=None):
-        todo = self.get_object()
-        serializer = self.get_serializer(todo, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data)
-
-    @action(detail=True, methods=['put'])
-    def complete(self, request, pk=None):
-        todo = self.get_object()
-        todo.completed = True
-        todo.save()
-
-        return Response({'detail': 'Todo marked as completed'})
-
-    @action(detail=True, methods=['put'])
-    def undo(self, request, pk=None):
-        todo = self.get_object()
-        todo.completed = False
-        todo.save()
-
-        return Response({'detail': 'Undo action performed'})
+            return Response({'detail': 'Todo reordered successfully'})
+        return Response({'detail': 'Order not provided'}, status=status.HTTP_400_BAD_REQUEST)
